86e310b3c7540e85554e4c541ef9c81040443e00
beven.zhou@hotmail.com <754058639@qq.com>
2020-05-31 21:48:22 +0800

    删除loader内容处理部分

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 var NameResolve = require('./src/dependencies/NameResolve');
 
 module.exports = function (content) {
-  return content.replace(/_\//g, '').replace(/node_modules/g, NameResolve.nodeModulesName);
+  return content;
 }
