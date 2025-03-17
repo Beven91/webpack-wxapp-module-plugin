a0f0a4c9ea259fc2e15b38e57691907938f77f08
zhoubiwen <zhoubiwen@shantaijk.cn>
2025-03-17 14:37:14 +0800

    add: webpack sources support

--- src/helper/loadModule.js
+++ src/helper/loadModule.js
@@ -1,7 +1,12 @@
+const path = require('path');
 
 module.exports = function (id, ctx) {
   return new Promise((resolve, reject) => {
-    ctx.addDependency(id);
+    let filePath = id;
+    if (!path.isAbsolute(id)) {
+      filePath = path.join(path.dirname(ctx.resourcePath), id)
+    }
+    ctx.addDependency(filePath);
     ctx.loadModule(id, (err, src) => {
       return err ? reject(err) : resolve(src)
     });
